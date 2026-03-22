208c24e16d40569881c47c2290d34534d814a7b3
Harshad Bajirao Khatale <khataleharshad26@gmail.com>
2026-03-22 23:21:41 +0530

    done backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,12 +1,39 @@
 import os
+import time
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request  
+from app.routes.auth_routes import router as auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+app.include_router(auth_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 @app.get("/")
-def home():
-    return {"message": "API running 🚀"}
+async def home():
+    return {"message": "API running "}
+
+# This runs on EVERY request automatically
+@app.middleware("http")
+async def log_request_time(request: Request, call_next):
+    start_time = time.time()
+    
+    response = await call_next(request)
+    
+    end_time = time.time()
+    duration = (end_time - start_time) * 1000
+    
+    print(f"{request.method} {request.url.path} → {duration:.2f}ms")
+    
+    return response
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
